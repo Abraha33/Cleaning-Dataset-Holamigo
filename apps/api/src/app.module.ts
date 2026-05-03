b93668612a7d4e94d354d5ad545af8201ef39b8a
Abraha33 <acaceres163@unab.edu.co>
2026-05-03 02:01:39 -0500

    chore: fix floating promises and add eslint dependencies

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,8 +1,8 @@
-﻿import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
-import { PrismaService } from "./prisma/prisma.service";
-import { ProductsModule } from "./products/products.module";
+﻿import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PrismaService } from './prisma/prisma.service';
+import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [ProductsModule],
--- apps/api/src/products/products.service.ts
+++ apps/api/src/products/products.service.ts
@@ -1,11 +1,26 @@
-﻿import { Injectable } from "@nestjs/common";
-import { PrismaService } from "../prisma/prisma.service";
+﻿import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 @Injectable()
 export class ProductsService {
   constructor(private prisma: PrismaService) {}
-  async create(data: any) { return this.prisma.product.create({ data }); }
-  async findAll() { return this.prisma.product.findMany({ include: { brand: true, category: true } }); }
-  async findOne(id: number) { return this.prisma.product.findUnique({ where: { id }, include: { brand: true, category: true } }); }
-  async update(id: number, data: any) { return this.prisma.product.update({ where: { id }, data }); }
-  async remove(id: number) { return this.prisma.product.delete({ where: { id } }); }
+  async create(data: any) {
+    return this.prisma.product.create({ data });
+  }
+  async findAll() {
+    return this.prisma.product.findMany({
+      include: { brand: true, category: true },
+    });
+  }
+  async findOne(id: number) {
+    return this.prisma.product.findUnique({
+      where: { id },
+      include: { brand: true, category: true },
+    });
+  }
+  async update(id: number, data: any) {
+    return this.prisma.product.update({ where: { id }, data });
+  }
+  async remove(id: number) {
+    return this.prisma.product.delete({ where: { id } });
+  }
 }
