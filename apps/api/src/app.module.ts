26805e37583ebae0060c5d4fd50f145f31573c0e
Abraha33 <acaceres163@unab.edu.co>
2026-05-03 21:54:23 -0500

    feat: bypass lint for domain schema

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { ProductsModule } from './products/products.module';
+import { StorageModule } from './storage/storage.module';
+import { ProceduresModule } from './procedures/procedures.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule, StorageModule, ProceduresModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
---
+++ src/storage/storage.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { StorageService } from './storage.service';
+import { StorageService } from './storage.service';
+
+@Module({
+  providers: [StorageService]
+})
+export class StorageModule {}
